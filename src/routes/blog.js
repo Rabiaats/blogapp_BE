8e4d4cb372c03f2f1fff9871911008a97c10f474
Rabiaats <atesr782@gmail.com>
2025-02-05 13:43:09 +0300

    docs: blog router add postLike and getLike

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,13 +2,13 @@
 
 const router = require('express').Router();
 
-const { list, create, read, update, deletee } = require('../controllers/blog');
+const { list, create, read, update, deletee, getLike, postLike } = require('../controllers/blog');
 const { isLogin} = require('../middlewares/permissions')
 
 
 router.route('/').get(list).post(isLogin, create);
 
-router.route('/:id').get(isLogin, read).put(isLogin, update).patch(isLogin, update).delete(isLogin, deletee);
+router.route('/:id').get(isLogin, read).put(isLogin, update).patch(isLogin, update).delete(isLogin, deletee).getLike(getLike).postLike(isLogin, postLike);
 
 /* ------------------------------------------------------- */
 module.exports = router;
