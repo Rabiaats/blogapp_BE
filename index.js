168f5eae1bc061df0e403aab9812a39b5d136d77
Rabiaats <atesr782@gmail.com>
2025-02-18 12:09:55 +0300

    docs: index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express()
 
 // envVariables to process.env:
 require('dotenv').config()
-const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
 
 // asyncErrors to errorHandler:
@@ -67,7 +66,7 @@ app.use(require('./src/routes'))
 app.use(require('./src/middlewares/errorHandler'))
 
 // RUN SERVER:
-app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+app.listen(PORT, () => console.log(`${PORT}`))
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
