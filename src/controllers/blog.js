96005aeea7f9c0fa646110ed2125744da3329c61
Rabiaats <atesr782@gmail.com>
2025-02-18 11:23:37 +0300

    docs: blog model update

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -8,17 +8,42 @@
 const Blog  = require("../models/blog");
 const Comment  = require("../models/comment");
 const { NotFoundError } = require("../errors/customError");
-const { error } = require("console");
+const requestIP = require("request-ip");
+const encrypt = require("../helpers/passwordEncrypt");
 
 /* ------------------------------------------------------- */
 
 module.exports = {
 
     list: async (req, res) => {
+         /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "List Blogs"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
 
         if (req.query.author) {
             const data = await Blog.find({ userId: req.query.author }).populate([
-              { path: "userId", select: "firstName lastName image" },
+              "categoryId",
+                {
+                  path: "userId",
+                  select: "firstName lastName image",
+                },
+                {
+                  path: "comments",
+                  populate: {
+                    path: "userId",
+                    select: "firstName lastName image",
+                  },
+                },
             ]);
     
           res.status(200).send({
@@ -29,13 +54,20 @@ module.exports = {
             data,
           });
         } else {
-          const data = await res.getModelList(Blog, { isPublish: true }, [
-            "categoryId",
-            {
-              path: "userId",
-              select: "firstName lastName image",
-            }
-          ]);
+            const data = await res.getModelList(Blog, { isPublish: true }, [
+                "categoryId",
+                {
+                  path: "userId",
+                  select: "firstName lastName image",
+                },
+                {
+                  path: "comments",
+                  populate: {
+                    path: "userId",
+                    select: "firstName lastName image",
+                  },
+                },
+              ]);
     
           res.status(200).send({
             error: false,
@@ -45,39 +77,52 @@ module.exports = {
         }
     },
 
-    // CRUD ->
-
     create: async (req, res) => {
+        /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Create Blog"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+            }
+        */
 
-        // Login olmuşsa, userId'yi req.user'dan alalım. (session)
         req.body.userId = req.user._id
 
         const result = await Blog.create(req.body);
 
         res.send({
-            result,
+            error: false,
+            result
         });
     },
 
     read: async (req, res) => {
-        const blog = await Blog.findOne({ _id: req.params.id }).populate([
-            {path: 'userId', select: 'firstName lastName image'},
-            {path: 'categoryId', select: 'name'}
-        ]);
-        if (!result) {
-            throw new NotFoundError("No matching documents found");
-        };
+        /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Get Single Blog"
+        */
 
-        const comments = await res.getModelList(Comment, {blogId: req.params.id}, ['userId', 'categoryId']);
+        const ip = encrypt(requestIP.getClientIp(req));
 
-        const result = {blog,comments}
-
-
-        const isUser = result.countOfVisitors.map((userId) => userId == req.user._id);
-
-        if(!(result.userId == req.user._id) || !isUser){
-            result.countOfVisitors.push(req.user._id);
-        }
+        const result = await Blog.findOneAndUpdate(
+            { _id: req.params.id },
+            { $addToSet: { visitors: ip } },
+            { new: true }
+          ).populate(
+            {
+              path: "userId",
+              select: "username firstName lastName image",
+            },
+            {
+              path: "comments",
+              select: "_id comment blogId updatedAt",
+              populate: {
+                path: "userId",
+                select: "username firstName lastName image",
+              },
+            }
+          );
 
         res.send({
             isError: false,
@@ -86,16 +131,36 @@ module.exports = {
     },
 
     update: async (req, res) => {
+        /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Update Blog"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+            }
+        */
 
         if (!req.user.isAdmin) req.body.userId = req.user._id;
 
         const result = await Blog.updateOne(
             { _id: req.params.id },
             req.body,
-            { runValidators: true }
-        );
+            {runValidators:true}
+          ).populate(
+            {
+              path: "userId",
+              select: "username firstName lastName image",
+            },
+            {
+              path: "comments",
+              select: "_id comment blogId updatedAt",
+              populate: {
+                path: "userId",
+                select: "username firstName lastName image",
+              },
+            }
+          );
 
-        // //! güncellenmek istenen veri var ama güncelleme yapılmadı
         if (result.matchedCount > 0 && result.modifiedCount === 0) {
           return res.status(200).send({ message: "Document already up-to-date." });
         }
@@ -103,15 +168,20 @@ module.exports = {
         res.status(202).send({
             isError: false,
             result,
-            new: await Blog.findOne({ _id: req.params.postId }),
+            new: await Blog.findOne({ _id: req.params.id }),
         });
     },
 
     deletee: async (req, res) => {
+         /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Delete Blog"
+        */
 
-        if (!req.user.isAdmin) req.body.userId = req.user._id;
-
-        const result = await Blog.deleteOne({ _id: req.params.id });
+        const result = await Blog.deleteOne({
+            userId: req.user._id, 
+            _id: req.params.id 
+        });
 
         if (result.deletedCount === 0) {
             throw new NotFoundError("No matching documents found");
@@ -123,30 +193,36 @@ module.exports = {
         });
     },
 
-    getLike: async (req, res) => {
-        const data = await Blog.findOne({ _id: req.params.id });
-
-        res.status(data.likes ? 200 : 404).send({
-            isError: data.likes ? false : true,
-            data: data.likes
-        })
-    },
-
     postLike: async (req, res) => {
-        const data = await Blog.findOne({ _id: req.params.id });
+         /*
+            #swagger.tags = ["Blogs"]
+            #swagger.summary = "Add/Remove Like"
+        */
 
-        if (!data.likes.find(id =>  id == res.user._id)){
-            data.likes.push(res.user._id);
-            res.status(200).send({
-                isError: false,
-                data
-            })
-        }else{
-            data.likes = data.likes.map(id => id != res.user._id);
-            res.status(200).send({
-                isError: false,
-            })
+        const blog = await Blog.findOne({ _id: req.params.id });
+
+        if(!blog){
+            return res.status(404).send({
+                error: true,
+                message: "Blog not found",
+            });
         }
+
+        const like = blog.likes.includes(req.user._id);
+
+        const update = like
+            ? { $pull: { likes: req.user._id } }
+            : { $addToSet: { likes: req.user._id } };
+
+        const data = await Blog.findByIdAndUpdate(req.params.id, update, {
+            new: true,
+        }).select("likes");
+
+        res.status(200).send({
+            error: false,
+            userLike: !like,
+            data
+          });
     }
 };
 
